100d15b8b52deec0efc053849c1fa8b56df778e5
Ionel Gog <gogionel@gmail.com>
2019-12-26 17:11:36 +0200

    Use relative import paths.

--- deepsort.py
+++ deepsort.py
@@ -1,8 +1,8 @@
-from deep_sort.deep_sort import nn_matching
-from deep_sort.deep_sort.tracker import Tracker 
-from deep_sort.application_util import preprocessing as prep
-from deep_sort.application_util import visualization
-from deep_sort.deep_sort.detection import Detection
+from .deep_sort.deep_sort import nn_matching
+from .deep_sort.deep_sort.tracker import Tracker 
+from .deep_sort.application_util import preprocessing as prep
+from .deep_sort.application_util import visualization
+from .deep_sort.deep_sort.detection import Detection
 
 import numpy as np
 
